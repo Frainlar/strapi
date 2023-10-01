bbc6c6d9c921aff2b90ad595bf8bd92dba3faee8
root <root@strapi.us-central1-a.c.ktern-194016.internal>
2023-10-01 04:54:10 +0000

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b063577fc3bf402d563a01dbd4a488c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd812fe2c-6635-487e-a522-22ba3ef18b81'
+};
